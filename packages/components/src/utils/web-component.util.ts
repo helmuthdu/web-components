e381f948f6ddaa9683fd1ab9c00852b8945303d4
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-12-24 18:02:09 +0100

    feat: update button component

--- packages/components/src/utils/web-component.util.ts
+++ packages/components/src/utils/web-component.util.ts
@@ -1,44 +1,122 @@
-export abstract class WebComponentHTMLElement<T extends Record<string, any>> extends HTMLElement {
-  #props!: T;
-  #eventList: { el: HTMLElement; event: string; action: (...args: any) => void }[] = [];
+import htm from 'htm';
+import vhtml from 'vhtml';
 
-  constructor() {
-    super();
+type DefineOptions<T> = {
+  props: T;
+  render: (props: T) => string | string[];
+  onUpdate?: (name: keyof T, prev: string, curr: string) => boolean | void;
+  onMounted?: () => void;
+  onRemoved?: () => void;
+};
+
+type EventItem = { el: HTMLElement; event: string; callback: (...args: any) => void };
+
+let currentlyRenderedInstance: any;
+
+const eventList: Map<any, Set<EventItem>> = new Map();
+
+export const event = (selector: string | HTMLElement, event: string, callback: (...args: any) => void) => {
+  const el =
+    typeof selector === 'string'
+      ? (currentlyRenderedInstance?.shadowRoot?.querySelector(selector) as HTMLElement)
+      : selector;
+
+  if (!eventList.has(currentlyRenderedInstance.token)) {
+    eventList.set(currentlyRenderedInstance.token, new Set<EventItem>());
   }
+  eventList.get(currentlyRenderedInstance.token)?.add({ el, event, callback });
 
-  #generateTemplate(): HTMLTemplateElement {
-    const template = document.createElement('template');
-    template.innerHTML = this.render(this.#props);
-    return template;
-  }
+  el.addEventListener(event, callback);
+};
 
-  disconnectedCallback() {
-    this.#eventList.forEach(({ el, event, action }) => {
-      el.addEventListener(event, action);
-    });
-  }
+export const emit = (event: string, detail: any) => {
+  currentlyRenderedInstance.dispatchEvent(new CustomEvent(event, { detail }));
+};
 
-  abstract render(props: T): string;
+export const useElementRef = (selector: string) => {
+  return currentlyRenderedInstance?.shadowRoot?.querySelector(`*[ref="${selector}"]`);
+};
 
-  addEvent(name: string, event: string, action: (...args: any) => void) {
-    const el = this.shadowRoot?.querySelector(name) as HTMLElement;
-    el.addEventListener(event, action);
-    this.#eventList.push({ el, event, action });
-  }
+export const define = <T>(name: string, { props, render, onUpdate, onMounted, onRemoved }: DefineOptions<T>) => {
+  customElements.define(
+    name,
+    class extends HTMLElement {
+      token = Symbol(name);
+      props: T;
 
-  fire(eventType: string, detail: any) {
-    this.dispatchEvent(new CustomEvent(eventType, { detail }));
-  }
+      #updateContent() {
+        if (this.shadowRoot) {
+          const content = render(props);
+          this.shadowRoot.innerHTML = Array.isArray(content) ? content.join('') : content;
+        }
+      }
 
-  init({ props }: { props: T }) {
-    this.#props = props;
-    const template = this.#generateTemplate();
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot?.appendChild(template.content.cloneNode(true));
-  }
+      static get observedAttributes() {
+        return [...Object.keys(props)];
+      }
 
-  hasProp(name: keyof T) {
-    const prop = this.#props[name];
-    return prop === '' || prop;
-  }
-}
+      constructor() {
+        super();
+
+        const self = this;
+
+        // @ts-ignore
+        this.props = new Proxy(props, {
+          get(target: T, key: keyof T) {
+            return target[key];
+          },
+          set(target: T, key: keyof T, val: any) {
+            if (val === '' || val) {
+              self.setAttribute(key as string, val);
+            } else {
+              self.removeAttribute(key as string);
+            }
+            target[key] = val;
+            return true;
+          }
+        });
+      }
+
+      connectedCallback() {
+        this.attachShadow({ mode: 'open' });
+
+        const template = document.createElement('template');
+        const content = render(props);
+        template.innerHTML = Array.isArray(content) ? content.join('') : content;
+        this.shadowRoot?.appendChild(template.content.cloneNode(true));
+
+        if (onMounted) {
+          currentlyRenderedInstance = this;
+          onMounted();
+        }
+      }
+
+      disconnectedCallback() {
+        eventList.get(this.token)?.forEach(({ el, event, callback }) => {
+          el.addEventListener(event, callback);
+        });
+
+        if (onRemoved) {
+          currentlyRenderedInstance = this;
+          onRemoved();
+        }
+      }
+
+      attributeChangedCallback(name: keyof T, prev: string, curr: string) {
+        // console.log(name, prev, curr);
+        if (prev !== curr) {
+          // @ts-ignore
+          this.props[name] = curr;
+          if (onUpdate) {
+            currentlyRenderedInstance = this;
+            if (onUpdate(name, prev, curr)) {
+              this.#updateContent();
+            }
+          }
+        }
+      }
+    }
+  );
+};
+
+export const html = htm.bind(vhtml);
--- packages/components/vite.config.js
+++ packages/components/vite.config.js
@@ -1,13 +1,24 @@
-const path = require('path');
-const { defineConfig } = require('vite');
+import pluginMetaUrl from '@uppercod/vite-meta-url';
+import loadCss from '@uppercod/vite-meta-url-load-css';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['./tailwindcss.css'],
   build: {
+    target: 'esnext',
+    polyfillModulePreload: false,
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      name: 'Tailwind',
+      entry: './src/main.ts',
+      name: 'tailwind-components',
       fileName: format => `tailwind.${format}.js`
     }
-  }
+  },
+  css: false,
+  plugins: [
+    pluginMetaUrl({
+      css: loadCss(),
+      md: true
+    })
+  ]
 });
