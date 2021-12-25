e47c33bd5f16a7cb1f4866a44e87c50f5b6fe225
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-12-25 21:13:47 +0100

    chore: update spacing type

--- packages/components/src/types.ts
+++ packages/components/src/types.ts
@@ -42,6 +42,41 @@ export type TextColor =
 
 export type Sizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-export type Spacing = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+export type Spacing =
+  | 0
+  | 'px'
+  | 0.5
+  | 1
+  | 1.5
+  | 2
+  | 2.5
+  | 3
+  | 3.5
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 14
+  | 16
+  | 20
+  | 24
+  | 28
+  | 32
+  | 36
+  | 40
+  | 44
+  | 48
+  | 52
+  | 56
+  | 60
+  | 64
+  | 72
+  | 80
+  | 96;
 
 export type Gap = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
