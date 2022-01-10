0254cfef392d20bf9f5c8e06ed2f36cf140066d1
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2022-01-10 12:09:13 +0100

    chore: update build for split files

--- packages/custom-elements/vite.lib.config.js
+++ packages/custom-elements/vite.lib.config.js
@@ -6,9 +6,20 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   assetsInclude: ['./tailwindcss.css'],
   build: {
+    outDir: 'lib',
     rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      },
       input: [
+        './src/components/accordion/accordion.ts',
+        './src/components/accordion/accordion-group.ts',
         './src/components/alert/alert.ts',
+        './src/components/avatar/avatar.ts',
+        './src/components/avatar/avatar-group.ts',
+        './src/elements/box/box.ts',
         './src/elements/button/button.ts',
         './src/elements/button/button-group.ts'
       ]
