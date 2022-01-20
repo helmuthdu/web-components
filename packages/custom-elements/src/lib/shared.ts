f54fe2e301f72058e6d71fc63b13e2f6d8196b5a
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2022-01-20 09:21:28 +0100

    feat: added isBoolean and isNumber helpers

--- packages/custom-elements/src/lib/shared.ts
+++ packages/custom-elements/src/lib/shared.ts
@@ -1,7 +1,9 @@
 export const isString = (arg: unknown) => typeof arg === 'string';
+export const isNumber = (arg: unknown) => typeof arg === 'number';
+export const isBoolean = (arg: unknown) => typeof arg === 'boolean';
 export const isObject = (arg: unknown) => typeof arg === 'object' && arg !== null;
-export const isFunction = (arg: unknown) => typeof arg === 'function';
 export const isArray = Array.isArray;
+export const isFunction = (arg: unknown) => typeof arg === 'function';
 export const isSVG = (tag: string) => {
   const regex = new RegExp(`^${tag}$`, 'i');
   return ['path', 'svg', 'use', 'g'].some(tag => regex.test(tag));
@@ -10,7 +12,5 @@ export const getAttrName = (prop: string) => prop.replace(/([A-Z])/g, '-$1').toL
 export const normalizeValue = (key: string | symbol, value: any) => {
   return key === 'dataset'
     ? Object.entries(value).reduce((acc, [k, v]) => ({ ...acc, [k]: v === '' ? true : v }), {} as typeof value)
-    : value === ''
-    ? true
     : value;
 };
