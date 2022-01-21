4f97331c34bad453e7aeb4859960b48e1608057f
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2022-01-21 18:55:05 +0100

    fix: failing to render some svg elements

--- packages/custom-elements/src/lib/shared.ts
+++ packages/custom-elements/src/lib/shared.ts
@@ -6,7 +6,7 @@ export const isArray = Array.isArray;
 export const isFunction = (arg: unknown) => typeof arg === 'function';
 export const isSVG = (tag: string) => {
   const regex = new RegExp(`^${tag}$`, 'i');
-  return ['path', 'svg', 'use', 'g'].some(tag => regex.test(tag));
+  return ['path', 'circle', 'rect', 'svg', 'use', 'g'].some(tag => regex.test(tag));
 };
 export const getAttrName = (prop: string) => prop.replace(/([A-Z])/g, '-$1').toLowerCase();
 export const normalizeValue = (key: string | symbol, value: any) => {
