36c70cf63c650f9a0470bf9f42ffacd9e8534d4e
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2022-01-25 09:19:36 +0100

    chore: update theme

--- packages/custom-elements/.storybook/theme.js
+++ packages/custom-elements/.storybook/theme.js
@@ -9,5 +9,8 @@ export default create({
   // UI
   appBg: '#171717',
   appContentBg: '#262626',
-  appBorderColor: '#3f3f46'
+  appBorderColor: '#3f3f46',
+
+  brandTitle: 'UI Library Kit',
+  brandImage: undefined,
 });
