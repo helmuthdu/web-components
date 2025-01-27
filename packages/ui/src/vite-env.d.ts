e7006bf82db44e3368f5753f30208dc4f64c8465
Helmuth Saatkamp <helmuthdu@gmail.com>
2025-01-27 23:28:45 +0100

    fix: linter issues

--- packages/ui/src/vite-env.d.ts
+++ packages/ui/src/vite-env.d.ts
@@ -49,8 +49,8 @@ type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends (infer U)[]
     ? DeepPartial<U>[]
     : T[P] extends readonly (infer U)[]
-    ? readonly DeepPartial<U>[]
-    : DeepPartial<T[P]>;
+      ? readonly DeepPartial<U>[]
+      : DeepPartial<T[P]>;
 };
 
 type Entries<T> = {
